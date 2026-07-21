bfe70c347b3b722eb7a95452770f23d0025a4656
Jinghao Hu <731409213@qq.com>
2026-07-22 03:32:55 +0800

    Export network distance and traversal APIs

--- src/pykdex/network/__init__.py
+++ src/pykdex/network/__init__.py
@@ -1,9 +1,18 @@
 # SPDX-FileCopyrightText: 2026 Jinghao Hu
 # SPDX-License-Identifier: MIT
 
-"""Geometric linear-network data foundations."""
+"""Geometric linear-network data and distance foundations."""
 
 from pykdex.network.dataset import NetworkDataset
+from pykdex.network.distance import (
+    NetworkDistanceAsset,
+    NetworkLocations,
+    TraversalResult,
+    TraversalState,
+    build_event_lixel_distances,
+    build_network_distance_asset,
+    truncated_traversal,
+)
 from pykdex.network.events import NetworkEvents, SnapResult, snap_events
 from pykdex.network.linear_network import LinearNetwork
 from pykdex.network.support import LixelSupport
@@ -17,4 +26,11 @@ __all__ = [
     "LixelSupport",
     "NetworkWorkspace",
     "NetworkDataset",
+    "NetworkLocations",
+    "NetworkDistanceAsset",
+    "build_network_distance_asset",
+    "build_event_lixel_distances",
+    "TraversalState",
+    "TraversalResult",
+    "truncated_traversal",
 ]
