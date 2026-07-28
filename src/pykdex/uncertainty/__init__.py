9e30160e31651e02b3ee5901addb7555cfc6b8b5
Jinghao Hu <731409213@qq.com>
2026-07-28 23:01:15 +0800

    Expose bootstrap plan from uncertainty namespace

---
+++ src/pykdex/uncertainty/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Jinghao Hu
+# SPDX-License-Identifier: MIT
+
+"""Empirical uncertainty contracts for pyKDEX fields."""
+
+from pykdex.uncertainty.plan import BootstrapPlan
+
+__all__ = ["BootstrapPlan"]
