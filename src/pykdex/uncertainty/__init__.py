40229934fab899be025dc94e8df18c3d42d9ba7a
Jinghao Hu <731409213@qq.com>
2026-07-28 23:48:54 +0800

    Expose spatial Bootstrap API

--- src/pykdex/uncertainty/__init__.py
+++ src/pykdex/uncertainty/__init__.py
@@ -9,10 +9,14 @@ from pykdex.uncertainty.fields import (
     pointwise_percentile_interval,
 )
 from pykdex.uncertainty.plan import BootstrapPlan
+from pykdex.uncertainty.results import BootstrapResult
+from pykdex.uncertainty.spatial import bootstrap_kde
 
 __all__ = [
     "BootstrapPlan",
+    "BootstrapResult",
     "FieldEnsemble",
     "PointwiseInterval",
+    "bootstrap_kde",
     "pointwise_percentile_interval",
 ]
